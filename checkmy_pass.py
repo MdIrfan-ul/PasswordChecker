5d6a3d4c7883d087b0f813bdd224f6af38be951f
MdIrfan-ul <88799245+MdIrfan-ul@users.noreply.github.com>
2021-08-19 21:27:49 +0530

    Add files via upload

---
+++ checkmy_pass.py
@@ -0,0 +1,38 @@
+import requests
+import hashlib
+import sys
+
+def api_data(query_char):
+  url = 'https://api.pwnedpasswords.com/range/' + query_char
+  res = requests.get(url)
+  if res.status_code!= 200:
+    raise RuntimeError(f'Error Fetching:{res.status_code}, check the api and try again')
+  return res
+
+def get_leaked_pass_count(hashes, hashes_for_the_checking):
+  hashes = (lines.split(':') for lines in hashes.text.splitlines())
+  for h, c in hashes:
+    if h == hashes_for_the_checking:
+    	return c
+  return 0  	
+
+def pwned_passwords(password):
+  sha1pass = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+  first5, last = sha1pass[:5], sha1pass[5:]
+  response = api_data(first5 )
+  return get_leaked_pass_count(response, last)
+
+def main(args):
+  for password in args:
+    c = pwned_passwords(password)
+    if c:
+     print(f'{password} was found {c} You Should Probably change it!!!')
+    else:
+     print(f'{password} NOT FOUND Carry On!')    
+  return 'done!'
+
+if __name__ == '__main__':
+  with open('InputPassword.txt', 'r') as file:
+  	lines = file.readlines()
+  	main(lines)  	
+  main(sys.argv[1:])
